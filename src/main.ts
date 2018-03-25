7cc47a0be4b94c8e47a61815406fd9ad5558146e
Ivo Meixner <ivo.meixner@se7en.cz>
2018-03-25 18:09:34 +0200

    Created core webpage structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+const F5_KEY: number = 116;
+
+const elemCode: HTMLTextAreaElement = document.getElementById("code") as HTMLTextAreaElement;
+const elemConsole: HTMLTextAreaElement = document.getElementById("console") as HTMLTextAreaElement;
+
+function compile(code: string): string {
+    return "Hello World!\n" + code;
+}
+
+let f5Down: boolean = false;
+
+elemCode.addEventListener("keydown", (e: KeyboardEvent): void => {
+    if ((e.which || e.keyCode) === F5_KEY) {
+        e.preventDefault();
+
+        if (!f5Down) {
+            f5Down = true;
+            elemConsole.innerHTML = compile(elemCode.value);
+            elemConsole.scrollTop = elemConsole.scrollHeight;
+            console.log("Compiled!");
+        }
+    }
+});
+
+elemCode.addEventListener("keyup", (e: KeyboardEvent): void => {
+    if ((e.which || e.keyCode) === F5_KEY) {
+        e.preventDefault();
+        f5Down = false;
+    }
+});
